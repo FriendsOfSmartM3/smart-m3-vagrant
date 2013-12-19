76f72d402d65af49ccf4488e6872016bf294a45b
Davide Bellettini <davide.bellettini@gmail.com>
2013-12-19 11:11:57 +0100

    Removing dangling reference

--- vagrant/puppet/manifests/smartm3.pp
+++ vagrant/puppet/manifests/smartm3.pp
@@ -1,8 +1,6 @@
 import 'preliminary.pp'
 import 'redsib.pp'
 
-File['/etc/init.d/report-service'] -> Service['report-service']
-
 exec {'download-redsib':
     command => '/usr/bin/wget http://garr.dl.sourceforge.net/project/smart-m3/Smart-M3-RedSIB_0.9/redsib_0.9.tar.gz -O redsib.tar.gz',
     creates => '/home/vagrant/redsib.tar.gz'
